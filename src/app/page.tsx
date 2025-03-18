df778fb3137b24a4f827f0768945c8ad72acfe00
suvaranjan <suvaranjan01@gmail.com>
2025-03-18 21:15:10 +0530

    new theme added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,14 +35,17 @@ export default function Home() {
           {/* Read Now Sheet */}
           <Sheet open={isReadSheetOpen} onOpenChange={setIsReadSheetOpen}>
             <SheetTrigger asChild>
-              <Button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto">
-                <Image
+              <Button
+                className="rounded-full p-5"
+                // className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+              >
+                {/* <Image
                   aria-hidden
                   src="/file.svg"
                   alt="File icon"
                   width={16}
                   height={16}
-                />
+                /> */}
                 Read now
               </Button>
             </SheetTrigger>
@@ -58,7 +61,8 @@ export default function Home() {
           <Sheet open={isTestSheetOpen} onOpenChange={setIsTestSheetOpen}>
             <SheetTrigger asChild>
               <Button
-                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+                className="rounded-full p-5"
+                // className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
                 variant="outline"
               >
                 Take Test
